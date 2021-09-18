e509270ea7aec190568c55db46809a1baac61935
Francis Li <mail@francisli.com>
2021-09-17 18:39:38 -0700

    Buttons

--- angular/projects/design/src/app/app-routing.module.ts
+++ angular/projects/design/src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { ButtonsComponent } from './buttons.component';
 import { IconsComponent } from './icons.component';
 import { InputsComponent } from './inputs.component';
 
 const routes: Routes = [
+  {
+    path: 'buttons',
+    component: ButtonsComponent,
+  },
   {
     path: 'icons',
     component: IconsComponent,
--- angular/projects/design/src/app/app.module.ts
+++ angular/projects/design/src/app/app.module.ts
@@ -3,13 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ButtonsComponent } from './buttons.component';
 import { IconsComponent } from './icons.component';
 import { InputsComponent } from './inputs.component';
 
 import { ComponentsModule } from '../components/components.module';
 
 @NgModule({
-  declarations: [AppComponent, IconsComponent, InputsComponent],
+  declarations: [AppComponent, ButtonsComponent, IconsComponent, InputsComponent],
   imports: [BrowserModule, AppRoutingModule, ComponentsModule],
   providers: [],
   bootstrap: [AppComponent],
