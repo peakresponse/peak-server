52ecbc626b648bff4442cd022590ea3bf7ea43a0
Francis Li <mail@francisli.com>
2021-09-21 16:11:20 -0700

    Setting up new onboarding

--- routes/onboarding.js
+++ routes/onboarding.js
@@ -1,10 +1,21 @@
 const express = require('express');
+const fs = require('fs');
+const path = require('path');
 
 const router = express.Router();
 
 /* GET onboarding SPA index file */
-router.get('/*', (req, res) => {
+router.get('/old/*', (req, res) => {
   res.render('onboarding');
+});
+
+router.get('/*', (req, res) => {
+  req.logout();
+  res.locals.designWebpackStats = JSON.parse(fs.readFileSync(path.join(__dirname, '../angular/projects/design/webpack-stats.json')));
+  res.locals.webpackStats = JSON.parse(fs.readFileSync(path.join(__dirname, '../angular/projects/onboarding/webpack-stats.json')));
+  res.render('onboarding/index', {
+    layout: 'onboarding/layout',
+  });
 });
 
 module.exports = router;
