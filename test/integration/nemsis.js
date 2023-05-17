9cc49c4a98868016dae85a42a473a5e31920de07
Francis Li <mail@francisli.com>
2023-05-16 18:18:07 -0700

    Remove deprecated public nemsis route, add new versioned XSD conversion path

---
+++ test/integration/nemsis.js
@@ -0,0 +1,30 @@
+const assert = require('assert');
+const HttpStatus = require('http-status-codes');
+const session = require('supertest-session');
+
+require('../helpers');
+const app = require('../../app');
+const nemsisPublic = require('../../lib/nemsis/public');
+
+describe('/nemsis', () => {
+  let testSession;
+
+  describe('GET /public/:nemsisVersion/:xsd', () => {
+    it('should return the JSON version of the specified NEMSIS XSD', async () => {
+      testSession = session(app);
+
+      await nemsisPublic.pull();
+      const repo = nemsisPublic.getNemsisPublicRepo('3.5.0.211008CP3');
+      await repo.pull();
+
+      let response;
+      // first call will convert to json on the fly
+      response = await testSession.get('/nemsis/public/3.5.0.211008CP3/dAgency_v3.xsd').expect(HttpStatus.OK);
+      assert.deepStrictEqual(response.body?.['xs:schema']?._attributes?.targetNamespace, 'http://www.nemsis.org');
+
+      // test again, to make sure that serving exiting file works
+      response = await testSession.get('/nemsis/public/3.5.0.211008CP3/dAgency_v3.xsd').expect(HttpStatus.OK);
+      assert.deepStrictEqual(response.body?.['xs:schema']?._attributes?.targetNamespace, 'http://www.nemsis.org');
+    });
+  });
+});
