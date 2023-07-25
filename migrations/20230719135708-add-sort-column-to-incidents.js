19d3a44726cfae79f752d70fe0b13764a2b7cd32
Francis Li <mail@francisli.com>
2023-07-24 17:35:04 -0700

    Update migration to set initial sort

--- migrations/20230719135708-add-sort-column-to-incidents.js
+++ migrations/20230719135708-add-sort-column-to-incidents.js
@@ -5,6 +5,19 @@ module.exports = {
       allowNull: false,
       defaultValue: 0,
     });
+    await queryInterface.sequelize.query(`
+      UPDATE incidents
+      SET sort=NULLIF(
+        regexp_replace(
+          substring(incidents.number for
+            case when position('-' in incidents.number)=0
+            then length(incidents.number)
+            else position('-' in incidents.number)-1
+            end),
+          '\\D', '', 'g'
+        ),
+        '')::int
+    `);
   },
 
   async down(queryInterface, Sequelize) {
