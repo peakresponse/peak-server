9ea5d6ae8ff35e9718b4a62f113bd307c460aade
Francis Li <francisli@users.noreply.github.com>
2023-08-30 18:07:38 -0700

    [Closes #233, Closes #237] XSD field refactor to include attribute editing (#238)
    * Starting work on attribute selection/input
    
    * Setting up other attributes
    
    * Setting up for attribute input
    
    * First pass, successful attribute editing
    
    * More styling tweaks
    
    * Handle simpletype union enumeration in attribute
    
    * Tweak multi value handling with attributes
    
    * Remove attributes when none left after deleting
    
    * Cleaning up array/form-field styles, xsd element name refactor started
    
    * Finish array-field style deprecation
    
    * XSD element refactor
    
    * Fix display text feedback margins
    
    * Support attributes with primitive types

---
+++ angular/projects/shared/src/lib/components/xsd/elements/xsd-element-multi.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+import { XsdElementBaseComponent } from './xsd-element-base.component';
+
+@Component({
+  selector: 'shared-xsd-element-multi',
+  templateUrl: './xsd-element-multi.component.html',
+})
+export class XsdElementMultiComponent extends XsdElementBaseComponent {}
