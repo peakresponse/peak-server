2479caa10f5b52bde575ba4f0077bf3cbc74ec84
Francis Li <mail@francisli.com>
2023-10-09 16:16:24 -0700

    Update master PSAP registry link

--- test/mocks/fcc.js
+++ test/mocks/fcc.js
@@ -4,16 +4,10 @@ const path = require('path');
 // Uncomment line below to record external HTTP calls
 // nock.recorder.rec();
 
-function mockDownloads(files) {
-  for (const file of files) {
-    nock('https://www.fcc.gov:443', { encodedQueryParams: true })
-      .get(`/file/${file.number}/download`)
-      .replyWithFile(200, path.resolve(__dirname, 'fcc', file.path));
-  }
-}
-
 function mockPsapRegistryDownloads() {
-  mockDownloads([{ number: '21421', path: 'masterpsapregistryv2.253.xlsx' }]);
+  nock('https://www.fcc.gov:443', { encodedQueryParams: true })
+    .get('/sites/default/files/masterpsapregistryv2.272.xlsx')
+    .replyWithFile(200, path.resolve(__dirname, 'fcc', 'masterpsapregistryv2.272.xlsx'));
 }
 
 module.exports = {
