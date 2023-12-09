2137bb411f8a4f6b2dce6901ac617be25e8f1aa2
Francis Li <mail@francisli.com>
2023-12-08 16:10:50 -0800

    Fix User/Assignment handling

--- angular/projects/shared/src/lib/components/headers/command-header.component.ts
+++ angular/projects/shared/src/lib/components/headers/command-header.component.ts
@@ -6,6 +6,7 @@ import { Component, ContentChild, EventEmitter, Input, Output, TemplateRef } fro
 })
 export class CommandHeaderComponent {
   @Input() user?: any;
+  @Input() vehicle?: any;
   @Input() showSearch = false;
   @Output() searchValueChange = new EventEmitter<string>();
   @ContentChild(TemplateRef) template: TemplateRef<any> | null = null;
