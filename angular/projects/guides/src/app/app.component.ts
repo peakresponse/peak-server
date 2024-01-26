935e0e1096ac7062afa0476e7b157116e529c084
Francis Li <mail@francisli.com>
2024-01-26 10:50:51 -0800

    Setting up Guides page layout

--- angular/projects/guides/src/app/app.component.ts
+++ angular/projects/guides/src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 
+import { AgencyService, NotificationService, UserService } from 'shared';
+
 @Component({
   selector: 'guides-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'guides';
+  constructor(
+    public currentAgency: AgencyService,
+    public currentUser: UserService,
+    public notification: NotificationService,
+  ) {}
 }
--- angular/projects/guides/src/app/app.module.ts
+++ angular/projects/guides/src/app/app.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+import { AgencyService, NavigationService, NotificationService, SchemaService, UserService } from 'shared';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
-  providers: [],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, NgbModule],
+  providers: [AgencyService, NavigationService, NotificationService, SchemaService, UserService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
