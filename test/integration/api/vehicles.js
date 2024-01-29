f99889d88e2992381f5ad7c4120d976541a19378
Francis Li <mail@francisli.com>
2024-01-29 14:28:06 -0800

    Fix test to match new behavior

--- test/integration/api/vehicles.js
+++ test/integration/api/vehicles.js
@@ -31,15 +31,13 @@ describe('/api/vehicles', () => {
   });
 
   describe('GET /', () => {
-    it('returns a list of Vehicles for the current agency', async () => {
+    it('returns a list of active Vehicles for the current agency', async () => {
       const response = await testSession.get('/api/vehicles').set('Host', `bmacc.${process.env.BASE_HOST}`).expect(StatusCodes.OK);
       const records = response.body;
-      assert.deepStrictEqual(records.length, 5);
+      assert.deepStrictEqual(records.length, 3);
       assert.deepStrictEqual(records[0].number, '43');
       assert.deepStrictEqual(records[1].number, '50');
-      assert.deepStrictEqual(records[2].number, '55');
-      assert.deepStrictEqual(records[3].number, '88');
-      assert.deepStrictEqual(records[4].number, '92');
+      assert.deepStrictEqual(records[2].number, '88');
     });
   });
 });
