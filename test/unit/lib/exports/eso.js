844994056d588aec4b44e9b9a5b9a7c8f26f0d48
Francis Li <mail@francisli.com>
2024-02-12 16:58:41 -0800

    Started ESO export while still waiting for complete test setup

---
+++ test/unit/lib/exports/eso.js
@@ -0,0 +1,23 @@
+const assert = require('assert');
+
+const { EsoClient } = require('../../../../lib/exports/eso');
+
+describe('lib', () => {
+  describe('exports', () => {
+    describe('eso', () => {
+      describe('EsoClient', () => {
+        describe('.authenticate', () => {
+          it('authenticates with the ESO Partner API to retrieve credentials', async () => {
+            const client = new EsoClient('https://identity.stage.esosuite.net/auth/connect/token');
+            client.username = process.env.ESO_STAGING_USERNAME;
+            client.password = process.env.ESO_STAGING_PASSWORD;
+            const credentials = await client.authenticate();
+            assert(credentials?.access_token);
+            assert.deepStrictEqual(credentials?.token_type, 'Bearer');
+            assert.deepStrictEqual(credentials?.expires_in, 3600);
+          });
+        });
+      });
+    });
+  });
+});
