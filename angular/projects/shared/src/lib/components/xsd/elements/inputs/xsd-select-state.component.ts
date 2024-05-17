bbb4e6c1f12f8eb254adb91a6a3bde68596159cf
Francis Li <mail@francisli.com>
2024-05-17 10:18:32 -0700

    Replace state select with typeahead input

--- angular/projects/shared/src/lib/components/xsd/elements/inputs/xsd-select-state.component.ts
+++ angular/projects/shared/src/lib/components/xsd/elements/inputs/xsd-select-state.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { HttpResponse } from '@angular/common/http';
+import { NgbTypeahead } from '@ng-bootstrap/ng-bootstrap';
+import { Observable, OperatorFunction, Subject, merge } from 'rxjs';
+import { debounceTime, distinctUntilChanged, filter, map } from 'rxjs/operators';
 
 import { XsdElementBaseComponent } from '../xsd-element-base.component';
 
@@ -9,26 +13,61 @@ let STATES: any[];
   templateUrl: './xsd-select-state.component.html',
 })
 export class XsdSelectStateComponent extends XsdElementBaseComponent {
+  @ViewChild('instance', { static: true }) instance?: NgbTypeahead;
+  model: any;
+
+  focus$ = new Subject<string>();
+  click$ = new Subject<string>();
+
   STATES: any[] = [];
+
+  search: OperatorFunction<string, readonly any[]> = (text$: Observable<string>) => {
+    const debouncedText$ = text$.pipe(debounceTime(200), distinctUntilChanged());
+    const clicksWithClosedPopup$ = this.click$.pipe(filter(() => !this.instance?.isPopupOpen()));
+    const inputFocus$ = this.focus$;
+
+    return merge(debouncedText$, inputFocus$, clicksWithClosedPopup$).pipe(
+      debounceTime(200),
+      distinctUntilChanged(),
+      map((term: string) =>
+        (term === '' ? STATES : STATES.filter((v) => v.name.toLowerCase().indexOf(term.toLowerCase()) > -1)).slice(0, 10),
+      ),
+    );
+  };
+
+  formatter = (x: any) => x.name;
 
   ngOnInit() {
     if (STATES) {
       this.STATES = STATES;
     } else {
-      this.api.states.index().subscribe((res) => {
+      this.api.states.index().subscribe((res: HttpResponse<any>) => {
         STATES = res.body;
+        Object.freeze(STATES);
         this.STATES = STATES;
       });
     }
   }
 
-  get enumeratedValue(): string | null {
-    let result: string | null = null;
+  get state(): any {
     for (const state of this.STATES) {
       if (state.id === this.value) {
-        return state.name;
+        return state;
       }
     }
-    return result;
+    return this.model;
+  }
+
+  onChangeState(state: any) {
+    this.model = state;
+    this.value = state?.id;
+  }
+
+  onBlur($event: any) {
+    console.log('onBlur', $event, this.value);
+    if (!this.value) {
+      this.model = undefined;
+      console.log('clearing model?');
+    }
   }
 }
