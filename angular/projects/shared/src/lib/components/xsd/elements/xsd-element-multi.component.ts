68cac0623b10beaa59b4ad716a7586b91bd327fb
Francis Li <mail@francisli.com>
2024-05-16 18:25:19 -0700

    [Closes #391] Set focus in new input field after Add

--- angular/projects/shared/src/lib/components/xsd/elements/xsd-element-multi.component.ts
+++ angular/projects/shared/src/lib/components/xsd/elements/xsd-element-multi.component.ts
@@ -1,9 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef } from '@angular/core';
 
 import { XsdElementBaseComponent } from './xsd-element-base.component';
+import { ApiService } from '../../../services/api.service';
 
 @Component({
   selector: 'shared-xsd-element-multi',
   templateUrl: './xsd-element-multi.component.html',
 })
-export class XsdElementMultiComponent extends XsdElementBaseComponent {}
+export class XsdElementMultiComponent extends XsdElementBaseComponent {
+  constructor(
+    private elementRef: ElementRef,
+    protected api: ApiService,
+  ) {
+    super(api);
+  }
+
+  onAdd() {
+    this.addValue({});
+    setTimeout(() => {
+      // for some reason querySelector :last-of-type selector not working, so fetching all
+      const items: HTMLElement[] = Array.from(this.elementRef?.nativeElement.querySelectorAll('.form-field__item') ?? []);
+      const item = items.pop();
+      (item?.querySelector('input, select') as HTMLElement)?.focus();
+    }, 100);
+  }
+}
