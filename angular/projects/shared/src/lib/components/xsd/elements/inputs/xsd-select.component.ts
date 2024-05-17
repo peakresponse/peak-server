27ad71d9499cca642fa5b8d9359dfd6ef6551112
Francis Li <mail@francisli.com>
2024-05-16 20:10:21 -0700

    Remove dead code

--- angular/projects/shared/src/lib/components/xsd/elements/inputs/xsd-select.component.ts
+++ angular/projects/shared/src/lib/components/xsd/elements/inputs/xsd-select.component.ts
@@ -7,27 +7,6 @@ import { XsdElementBaseComponent } from '../xsd-element-base.component';
   templateUrl: './xsd-select.component.html',
 })
 export class XsdSelectComponent extends XsdElementBaseComponent {
-  get minLength(): number | null {
-    if (this.type?.['xs:restriction']?.['xs:minLength']?._attributes?.value) {
-      return parseInt(this.type['xs:restriction']['xs:minLength']._attributes.value);
-    }
-    return null;
-  }
-
-  get maxLength(): number | null {
-    if (this.type?.['xs:restriction']?.['xs:maxLength']?._attributes?.value) {
-      return parseInt(this.type['xs:restriction']['xs:maxLength']._attributes.value);
-    }
-    return null;
-  }
-
-  get pattern(): string | null {
-    if (this.type?.['xs:restriction']?.['xs:pattern']?._attributes?.value) {
-      return this.type['xs:restriction']['xs:pattern']._attributes.value;
-    }
-    return null;
-  }
-
   onChange(newValue: string) {
     for (let item of this.type?.['xs:restriction']?.['xs:enumeration']) {
       const { value, nemsisCode } = item._attributes ?? {};
