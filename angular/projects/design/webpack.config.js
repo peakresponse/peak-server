35765d0818a3f3e5c235b5b115a54301a614f8e9
Francis Li <mail@francisli.com>
2025-02-19 13:00:47 -0800

    Upgrade to angular 17

--- angular/projects/design/webpack.config.js
+++ angular/projects/design/webpack.config.js
@@ -2,6 +2,13 @@ const BundleTracker = require('webpack-bundle-tracker');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
+  plugins: [
+    new MiniCssExtractPlugin(),
+    new BundleTracker({
+      path: __dirname,
+      filename: 'webpack-stats.json',
+    }),
+  ],
   module: {
     rules: [
       {
@@ -22,11 +29,4 @@ module.exports = {
   output: {
     publicPath: '/angular/design/',
   },
-  plugins: [
-    new MiniCssExtractPlugin(),
-    new BundleTracker({
-      path: __dirname,
-      filename: 'webpack-stats.json',
-    }),
-  ],
 };
