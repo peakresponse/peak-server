b8b06c0d9726764366db6e5445195ddc8c0ddc88
Francis Li <mail@francisli.com>
2025-05-27 17:27:24 -0700

    Style OAuth authorization page

--- routes/oauth.js
+++ routes/oauth.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const fs = require('fs');
+const path = require('path');
 const { StatusCodes } = require('http-status-codes');
 
 const models = require('../models');
@@ -8,6 +10,14 @@ const rollbar = require('../lib/rollbar');
 const interceptors = require('./interceptors');
 
 const router = express.Router();
+
+let designWebpackStats;
+function getDesignWebpackStats() {
+  if (!designWebpackStats || process.env.NODE_ENV !== 'production') {
+    designWebpackStats = JSON.parse(fs.readFileSync(path.join(__dirname, '../angular/projects/design/webpack-stats.json')));
+  }
+  return designWebpackStats;
+}
 
 router.post('/token', async (req, res) => {
   try {
@@ -28,6 +38,9 @@ router.get('/authorize', interceptors.requireLogin, async (req, res) => {
       clientId: req.query.client_id,
     },
   });
+  const cancelUri = new URL(locals.client.redirectUri);
+  locals.cancelUri = `${cancelUri.protocol}//${cancelUri.host}`;
+  locals.designWebpackStats = getDesignWebpackStats();
   res.render('oauth/authorize', locals);
 });
 
